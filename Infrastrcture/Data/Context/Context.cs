86e772d85910eb17f6ca295164f7e1cc7332c08b
mina <mrasi7306@gmail.com>
2022-09-06 16:32:53 +0430

    sql

--- Infrastrcture/Data/Context/Context.cs
+++ Infrastrcture/Data/Context/Context.cs
@@ -2,7 +2,8 @@ using Microsoft.EntityFrameworkCore;
 public class Context:DbContext
 {
     public DbSet<User> Users { get; set; }
-
+    public DbSet<MainQuestion> MainQuestions { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder db)
     {
         db.UseSqlServer("data source=.;initial catalog = DBQuestions;integrated security=true");
---
+++ Infrastrcture/Migrations/20220906115748_iii.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastrcture.Migrations
+{
+    public partial class iii : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MainQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionNumber = table.Column<int>(type: "int", nullable: false),
+                    CorrectAnswer = table.Column<int>(type: "int", nullable: false),
+                    Questinon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer3 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer4 = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MainQuestions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MainQuestions");
+        }
+    }
+}
